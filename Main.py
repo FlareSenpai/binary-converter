80a1b59b5eef0efc6c209dfa158fdeb4f7be7da2
Muhammad Taufiqurrahman <mhmtaufiqurrahman@gmail.com>
2022-05-23 18:52:35 +0700

    Menambahkan fitur auto copy

--- Main.py
+++ Main.py
@@ -1,15 +1,23 @@
+import pyperclip
+
 def binaryToStr():
     inputValue = input('Masukkan bilangan binary\n\n')
     
     result = "".join([chr(int(binary, 2)) for binary in inputValue.split(" ")])
+    pyperclip.copy(result)
+
     print('\n' + result + '\n')
+    print('\nHasilnya sudah dicopy ke clipboard\n')
     awalan()
 
 def strToBinary():
     inputValue = input('Masukkan text\n\n')
     
     result = ' '.join(format(ord(x), 'b') for x in inputValue)
+    pyperclip.copy(result)
+
     print('\n' + result + '\n')
+    print('\nHasilnya sudah dicopy ke clipboard\n')
     awalan()
 
 def awalan():
